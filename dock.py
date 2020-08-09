713ece9a04ce57e2d9ecb47aba98cb8d18a1d2b1
mbrockley1901 <mbrockley@middlebury.edu>
2020-08-09 11:22:12 -0400

    initial commit

---
+++ dock.py
@@ -0,0 +1,20 @@
+#iterative Autodock Vina
+import os
+
+#structures
+enzyme = "DesB_6XBBB.pdbqt" #enzyme must be in pdbqt format
+substrates = ["1_3_diaminopentane", "1_3_diaminopropane", "n_butylamine",
+	"ornithine", "spermidine", "lysine", "putrescine", "cadaverine"]
+
+#docking each substrate with DesB
+for ligand in substrates:
+	
+	logfile = "log_" + ligand + ".txt"
+	output = "vina_" + ligand + ".pdbqt"
+	command = "C:\Program Files (x86)\The Scripps Research Institute\Vina\'vina" + " --receptor " + enzyme + "--ligand " + ligand + ".pdbqt" " --config conf.txt --out " + output + " --log " + logfile
+	"""concatenates strings for execution"""
+	
+	#this command changes based on the location of vina and the Os of the system
+	os.system(command)
+	#executes command
+	
