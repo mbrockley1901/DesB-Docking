bdca8ab49746c6a69ed472df760a51a92c1162ae
mbrockley1901 <mbrockley@middlebury.edu>
2020-08-16 20:42:35 -0400

    adjusted ornithine and lysine for basic conditions

--- vinadock.sh
+++ vinadock.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#preparing receptor pdbqt file with MGLtools
+#python prepare_receptor4.py -r DesB_6XBBB.pdb -U 'waters' -A 'hydrogens'
+
+#preparing ligand pdbqt files
+for molecule in ligands/*.pdb; do
+	python prepare_ligand4.py -l ${molecule} -A 'hydrogens' -B
+done
+
+
+#docking execution
+for substrate in ./ligands/*.pdbqt; do
+	{vina_program_path} --receptor DesB_6XBBB.pdbqt --ligand $substrate --config conf.txt --out "./results/vina_${basename substrate}" --log "./logs/log_${basename -s .pdbqt substrate).txt"
+done
